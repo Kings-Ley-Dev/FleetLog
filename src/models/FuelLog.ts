e6d77f1a87840d080de0d836b921f57233278901
Kingsley Quarshie <kingsley.quarshie.01@gmail.com>
2026-09-17 12:45:28 +0000

    Final Commit

--- src/models/FuelLog.ts
+++ src/models/FuelLog.ts
@@ -1,4 +1,4 @@
-import { Schema, model, models, type Model, type Types } from "mongoose";
+import { Schema, model, models, type Model, type Types } from "mongoose"; 
 
 export interface IFuelLog {
   _id: Types.ObjectId;
